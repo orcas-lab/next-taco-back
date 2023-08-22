415b2d7077e780d422b732a1dccc24de05274c63
GaoNeng-wWw <gaonenggg@outlook.com>
2023-08-22 21:22:40 +0800

    🦄 refactor(common): microservice-exception

--- libs/common/src/microservice-exception.filter.ts
+++ libs/common/src/microservice-exception.filter.ts
@@ -1,6 +1,6 @@
 import { MicroserviceError } from '@app/errors/errors.service';
 import { MicroserviceErrorTable } from '@app/errors/microservice.error';
-import { Catch, ExceptionFilter, Logger } from '@nestjs/common';
+import { Catch, ExceptionFilter, Logger, ValidationPipe } from '@nestjs/common';
 import { Observable, throwError } from 'rxjs';
 
 @Catch()
@@ -14,3 +14,15 @@ export class MicroServiceExceptionFilter<T> implements ExceptionFilter {
         return throwError(() => exception);
     }
 }
+
+export const createValidationPipe = () =>
+    new ValidationPipe({
+        exceptionFactory(errors) {
+            const details = [];
+            for (let i = 0; i < errors.length; i++) {
+                const err = errors[i];
+                details.push(...Object.values(err.constraints));
+            }
+            throw MicroserviceErrorTable.PARAM_INVALIDATE(details);
+        },
+    });
