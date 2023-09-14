a7c9be4f807121e77e0590ccec9b8563b8df5949
GaoNeng-wWw <gaonenggg@outlook.com>
2023-09-14 14:06:23 +0800

    feat(request): refinment request controller

--- apps/request/src/request.controller.ts
+++ apps/request/src/request.controller.ts
@@ -1,7 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { RequestService } from './request.service';
+import {
+    AcceptRequestData,
+    DeleteRequestData,
+    ListRequestData,
+    MicroService_AddRequestData,
+    RefuseRequestData,
+    UpdateReuqestData,
+} from '@app/dto/request.dto';
 
 @Controller()
 export class RequestController {
     constructor(private readonly requestService: RequestService) {}
+    async ListReuqests(data: ListRequestData) {
+        return this.requestService.listReuqests(data);
+    }
+    async AddRequest(data: MicroService_AddRequestData) {
+        return this.requestService.add(data);
+    }
+    async DeleteRequest(data: DeleteRequestData) {
+        return this.requestService.delete(data);
+    }
+    async UpdateRequest(data: UpdateReuqestData) {
+        return this.requestService.update(data);
+    }
+    async AcceptRequest(data: AcceptRequestData) {
+        return this.requestService.accept(data);
+    }
+    async RefuseRequest(data: RefuseRequestData) {
+        return this.requestService.refuse(data);
+    }
 }
