19d342138657c3816da7e7854ce2fa63084e5bdb
GaoNeng-wWw <gaonenggg@outlook.com>
2023-09-20 13:21:57 +0800

    feat(gateway): user gateway

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
 import { AccountModule } from './account/account.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AccountModule],
+  imports: [AccountModule, UserModule],
   controllers: [GatewayController],
   providers: [GatewayService],
 })
