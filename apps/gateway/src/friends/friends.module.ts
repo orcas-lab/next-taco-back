3a10c97faa388a39ec8a2181a32f8e7152fa634f
GaoNeng-wWw <gaonenggg@outlook.com>
2023-09-24 13:29:53 +0800

    refactor: remove friends test file
    because controller just send microservice

--- apps/gateway/src/friends/friends.module.ts
+++ apps/gateway/src/friends/friends.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { FriendsService } from './friends.service';
 import { FriendsController } from './friends.controller';
 import { ClientsModule } from '@nestjs/microservices';
 import providers from '@app/clients-provider';
@@ -7,6 +6,5 @@ import providers from '@app/clients-provider';
 @Module({
     imports: [ClientsModule.register([providers.FRIEND_SERVICE])],
     controllers: [FriendsController],
-    providers: [FriendsService],
 })
 export class FriendsModule {}
