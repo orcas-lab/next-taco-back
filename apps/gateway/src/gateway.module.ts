1361890e8cd2488f9cf843ad6e0eed8d37bf222c
GaoNeng-wWw <gaonenggg@outlook.com>
2023-09-27 10:36:28 +0800

    feat(gateway): notice

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -4,8 +4,16 @@ import { UserModule } from './user/user.module';
 import { FriendsModule } from './friends/friends.module';
 import { ReputationModule } from './reputation/reputation.module';
 import { RequestModule } from './request/request.module';
+import { NoticeModule } from './notice/notice.module';
 
 @Module({
-    imports: [AccountModule, UserModule, FriendsModule, ReputationModule, RequestModule],
+    imports: [
+        AccountModule,
+        UserModule,
+        FriendsModule,
+        ReputationModule,
+        RequestModule,
+        NoticeModule,
+    ],
 })
 export class GatewayModule {}
